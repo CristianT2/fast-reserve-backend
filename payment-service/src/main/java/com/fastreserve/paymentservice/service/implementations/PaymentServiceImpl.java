13a8c21f46f4789fa80a14ed7cbb2d51c6253b0a
CristianT2 <c.torrejon96@gmail.com>
2026-02-06 16:22:52 -0300

    Se creo la capa service

---
+++ payment-service/src/main/java/com/fastreserve/paymentservice/service/implementations/PaymentServiceImpl.java
@@ -0,0 +1,73 @@
+package com.fastreserve.paymentservice.service.implementations;
+
+import com.fastreserve.paymentservice.client.ReservationClient;
+import com.fastreserve.paymentservice.dto.PaymentRequest;
+import com.fastreserve.paymentservice.dto.PaymentResponse;
+import com.fastreserve.paymentservice.dto.ReservationDTO;
+import com.fastreserve.paymentservice.entity.Payment;
+import com.fastreserve.paymentservice.entity.PaymentStatus;
+import com.fastreserve.paymentservice.mapper.PaymentMapper;
+import com.fastreserve.paymentservice.repository.PaymentRepository;
+import com.fastreserve.paymentservice.service.interfaces.IPaymentService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentServiceImpl implements IPaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper paymentMapper;
+    private final ReservationClient reservationClient;
+
+    @Override
+    @Transactional
+    public PaymentResponse processPayment(PaymentRequest request) {
+        log.info("Iniciando proceso de pago para la reserva: {}", request.getReservationId());
+        ReservationDTO reservation = reservationClient.getReservationById(request.getReservationId());
+
+        if(reservation == null){
+            throw new RuntimeException("No se encontró la reserva con ID: " + request.getReservationId());
+        }
+
+        if(reservation.getTotalPrice().compareTo(request.getAmount()) != 0){
+            log.error("El monto enviado ({}) no coincide con el de la reserva ({})",
+                    request.getAmount(), reservation.getTotalPrice());
+            throw new RuntimeException("El monto del pago no es correcto.");
+        }
+
+        Payment payment = paymentMapper.toEntity(request);
+        payment.setStatus(PaymentStatus.SUCCESS);
+        payment.setTransactionReference("TRX-" + UUID.randomUUID().toString().substring(0,8).toUpperCase());
+
+        Payment savedPayment = paymentRepository.save(payment);
+        log.info("Pago procesado exitosamente con ID: {}", savedPayment.getId());
+
+        return paymentMapper.toDTO(savedPayment);
+    }
+
+    @Override
+    @Transactional
+    public PaymentResponse getPaymentByReservationId(UUID reservationId) {
+        Payment payment = paymentRepository.findByReservationId(reservationId)
+                .orElseThrow(() -> new RuntimeException("Pago no encontrado para la reserva: " + reservationId));
+        return paymentMapper.toDTO(payment);
+    }
+
+    @Override
+    @Transactional()
+    public List<PaymentResponse> getAllPayments() {
+        log.info("Obteniendo lista de todos los pagos");
+        return paymentRepository.findAll()
+                .stream()
+                .map(paymentMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+}
